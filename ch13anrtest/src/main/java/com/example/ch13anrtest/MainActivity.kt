e6549f39499e4ef5f7d7046506173b321da3b899
vJEKKYv <wpdnjs20@gmail.com>
2024-04-12 14:45:37 +0900

    ch13ANRTest backgroundScopTest

--- ch13anrtest/src/main/java/com/example/ch13anrtest/MainActivity.kt
+++ ch13anrtest/src/main/java/com/example/ch13anrtest/MainActivity.kt
@@ -8,6 +8,13 @@ import kotlin.concurrent.thread
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ch13anrtest.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.consume
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.launch
 import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
@@ -16,23 +23,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        /*
         val handler = object : Handler(){
             override fun handleMessage(msg: Message) {
                 super.handleMessage(msg)
                 binding.result.text = "sum: ${msg.arg1}"
             }
         }
-
+        */
+        val channel = Channel<Int> ()
+        val backgroundScope = CoroutineScope(Dispatchers.Default)
         binding.click.setOnClickListener{
-            var sum = 0L
-            var time = measureTimeMillis {
-                for (i in 1..10){
-                    sleep(1000)
-                    sum +=i
+            backgroundScope.launch {
+                var sum = 0L
+                var time = measureTimeMillis {
+                    for (i in 1..10) {
+                        sleep(1000)
+                        sum += i
+                    }
                 }
+                Log.d("kkang", "time : $time")
+                channel.send(sum.toInt())
             }
-            Log.d("kkang", "time : $time")
+        }
+        val mainScope = GlobalScope.launch (Dispatchers.Main){
+            channel.consumeEach {
+                binding.result.text = "sum: $it"
+            }
         }
     }
 }
