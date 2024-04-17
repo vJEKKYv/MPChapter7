3e63be03198c4d5bfbb9e3e4977bd7c488b5961e
JEKKY <wpdnjs20@gmail.com>
2024-04-17 16:32:58 +0900

    ch11 menu resource로 메뉴 컨트롤하기

--- ch11/src/main/java/com/example/ch11/MainActivity.kt
+++ ch11/src/main/java/com/example/ch11/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.ch11
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,9 +17,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        title = "Two Activity"
-        binding.button.setOnClickListener{
+        title = "Main Activity"
+        binding.button.setOnClickListener {
             startActivity(Intent(this, TwoActivity::class.java))
         }
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menu1 -> startActivity(Intent(this, MainActivity2::class.java))
+            R.id.menu2 -> startActivity(Intent(this, MainActivity3::class.java))
+            R.id.menu3 -> Toast.makeText(this,"세번째 메뉴", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
